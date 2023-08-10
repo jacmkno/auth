36d6cc67e545270d94a5c2914e87882b1d04f4d9
jacmkno <jose@activisual.net>
2023-08-10 01:11:14 -0500

    CSS and div...

--- auth.js
+++ auth.js
@@ -1,4 +1,20 @@
 (()=>{
     if(location.hash != '#auth') return;
-    alert('auth module!');
+
+    (async link => {
+        console.log('XXXX:', link);
+        link.rel = 'stylesheet';
+        var urlObj = new URL(document.currentScript.src);
+        urlObj.pathname = urlObj.pathname.substring(0, urlObj.pathname.lastIndexOf('/') + 1) + 'auth.css'; // Replace 'yourfile.ext' with your filename
+
+        var absoluteURI = urlObj.toString();        
+        link.href = absoluteURI;
+        document.head.appendChild(link);
+    })(document.createElement('link'));
+
+    (async C => {
+        console.log('ZZZZ:', C);
+        C.className = 'auth';
+        C.innerText = 'Session tools...';
+    })(document.createElement('nav')).then(c => document.body.appendChild(c));
 })();
